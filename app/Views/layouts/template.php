f37f0a49cde5cca6ea4d912aac4dba91e659f56c
RHAZZHAMIR AGUILAR <aguilarrhazzhamir@gmail.com>
2025-05-09 22:17:25 +0800

    'home'

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -656,7 +656,7 @@
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="<?= base_url() ?>">Home</a></li>
+                                <li class="breadcrumb-item"><a href="<?= base_url('dashboard') ?>">Home</a></li>
                                 <li class="breadcrumb-item active"><?= $title ?? 'Dashboard' ?></li>
                             </ol>
                         </div>
